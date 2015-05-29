52cbb768f58d743e03b4e8bd64e818c609490a02
Michał Knapik <michal.knapik@u2i.com>
2015-05-29 10:56:56 +0200

    add Code Climate test coverage formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,14 @@ if ENV['COVERAGE']
   require 'simplecov'
   require 'simplecov-rcov'
   require 'simplecov-rcov-text'
+  require 'codeclimate-test-reporter'
 
   SimpleCov.add_filter '/spec/'
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
     SimpleCov::Formatter::RcovFormatter,
     SimpleCov::Formatter::RcovTextFormatter,
+    CodeClimate::TestReporter::Formatter,
   ]
   SimpleCov.start
 end
