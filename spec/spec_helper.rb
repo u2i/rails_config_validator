3413ea9a561476a517cbb2d2ea584e6bd57b8efd
Michał Knapik <michal.knapik@u2i.com>
2015-08-06 13:52:12 +0200

    Add ns-rake_tasks gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,6 @@
 require 'rubygems'
 
-if ENV['COVERAGE']
-  require 'simplecov'
-  require 'simplecov-rcov'
-  require 'simplecov-rcov-text'
-  require 'codeclimate-test-reporter'
-
-  SimpleCov.add_filter '/spec/'
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::RcovFormatter,
-    SimpleCov::Formatter::RcovTextFormatter,
-    CodeClimate::TestReporter::Formatter,
-  ]
-  SimpleCov.start
-end
+require 'ns/rake_tasks/coverage'
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rails_config_validator'
